659423769541169457c41f71c8a038e2d64ba079
Gioacchino Mazzurco <gio@retroshare.cc>
2022-01-11 12:25:41 +0100

    Furter cleanup after BitDHT split

--- src/util/bdstring.cc
+++ src/util/bdstring.cc
@@ -3,7 +3,7 @@
  *                                                                             *
  * BitDHT: An Flexible DHT library.                                            *
  *                                                                             *
- * Copyright (C) 2010 Retroshare Team <retroshare.project@gmail.com>           *
+ * Copyright (C) 2010 Retroshare Team <contact@retroshare.cc>           *
  *                                                                             *
  * This program is free software: you can redistribute it and/or modify        *
  * it under the terms of the GNU Affero General Public License as              *
